65eaa5266a03941eab8d76c0ea165b402b9ae769
Matt Ellen <matt@matthewellen.co.uk>
2015-04-17 00:19:08 +0100

    set the heights of the cards as well as the cells move the setCellHieghts call to a different module

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
 function setCellHeights()
 {
-     var cells = document.querySelectorAll('.cell');
-     var width = cells[0].clientWidth;
-     var height = 320/240 * width;
-     for(var ci = 0; ci < cells.length; ci++)
-     {
-	 cells[ci].height = height;
-     }
+    var cells = document.querySelectorAll('.cell');
+    var width = cells[0].clientWidth;
+    var height = 320/240 * width;
+    for(var ci = 0; ci < cells.length; ci++)
+    {
+	cells[ci].height = height;
+    }
+    var cards = document.querySelectorAll('img.card');
+    for(var ci = 0; ci < cards.length; ci++)
+    {
+	cards[ci].width = width;
+	cards[ci].height = height;
+    }
 }
 
 window.onresize = setCellHeights;
 
 (function()
- {
-     setCellHeights();
-     
+ {   
      var app = angular.module('freecell', ['freecell-deck']);
  })();
