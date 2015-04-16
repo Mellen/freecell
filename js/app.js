f87379441f57d071fc95b44b7bdcaa779216467f
Matt Ellen <matt@matthewellen.co.uk>
2015-04-16 12:59:17 +0100

    create table for free and home cells style the cells make the cells the rigth height

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+function setCellHeights()
+{
+     var cells = document.querySelectorAll('.cell');
+     var width = cells[0].clientWidth;
+     var height = 320/240 * width;
+     for(var ci = 0; ci < cells.length; ci++)
+     {
+	 cells[ci].height = height;
+     }
+}
+
+window.onresize = setCellHeights;
+
 (function()
  {
+     setCellHeights();
  })();
