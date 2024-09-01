5cc94ff7432766676acc7fc6da1c32068457c30c
Matt Ellen <127148+Mellen@users.noreply.github.com>
2024-09-01 20:20:29 +0100

    fixed typo

--- js/deck.js
+++ js/deck.js
@@ -124,9 +124,9 @@ const Game = (function()
      Game.prototype.generateHistoryCandidate = function()
      {
 	 let history = {
-	     table: this.tables.reduce((tbl, col) => { tbl.push([...col]), return tbl;}, [])
+	     table: this.tables.reduce((tbl, col) => { tbl.push([...col]); return tbl; }, [])
 	     freecells: [...this.freecells],
-	     home: this.home.reduce((home, col) => { home.push([...col]), return home;}, [])
+	     home: this.home.reduce((home, col) => { home.push([...col]); return home; }, [])
 	 };
 	 return history;
      }
